65844c487eb8836c0f88ffa18ab05382578ba9c1
Tarek-Ahmed-Helmy <tarek.ahmed35.01@gmail.com>
2025-02-01 01:44:39 +0200

    add customer section

---
+++ myshop.Web/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myshop.Entities.Repositories;
+using myshop.Entities.ViewModels;
+
+namespace myshop.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var products = _unitOfWork.Product.GetAll();
+            return View(products);
+        }
+
+        public IActionResult Details(int id) 
+        {
+            ShoppingCart shoppingCart = new ShoppingCart()
+            {
+                Product = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == id, includeEntity: "Category"),
+                Quantity = 1
+            };
+            return View(shoppingCart);
+        }
+    }
+}
